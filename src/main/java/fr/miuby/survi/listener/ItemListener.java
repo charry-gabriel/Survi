276aeef257609a651ab6cda560563bb0ca7e0d40
Ayfrino <Ayfrino@gmail.com>
2024-12-27 17:54:27 +0100

    Changement des crafts des armures

--- src/main/java/fr/miuby/survi/listener/ItemListener.java
+++ src/main/java/fr/miuby/survi/listener/ItemListener.java
@@ -22,6 +22,8 @@ public class ItemListener implements Listener {
         }
 
         ItemStack[] matrix = event.getInventory().getMatrix();
+
+        //region chainmail_armor
         if((matrix[3] != null && matrix[3].getType() == Material.LAVA_BUCKET
                 && matrix[5] != null && matrix[5].getType() == Material.LAVA_BUCKET
                 && matrix[6] != null && matrix[6].getType() == Material.LAVA_BUCKET
@@ -29,34 +31,25 @@ public class ItemListener implements Listener {
                 && (matrix[0] == null || matrix[0].getType() == Material.AIR)
                 && (matrix[1] == null || matrix[1].getType() == Material.AIR)
                 && (matrix[2] == null || matrix[2].getType() == Material.AIR)
-                && (matrix[4] == null || matrix[4].getType() == Material.AIR)
+                && matrix[4] != null && matrix[4].getType() == Material.LEATHER_BOOTS
                 && (matrix[7] == null || matrix[7].getType() == Material.AIR))
                 || (matrix[0] != null && matrix[0].getType() == Material.LAVA_BUCKET
                 && matrix[2] != null && matrix[2].getType() == Material.LAVA_BUCKET
                 && matrix[3] != null && matrix[3].getType() == Material.LAVA_BUCKET
                 && matrix[5] != null && matrix[5].getType() == Material.LAVA_BUCKET)
                 && (matrix[1] == null || matrix[1].getType() == Material.AIR)
-                && (matrix[4] == null || matrix[4].getType() == Material.AIR)
+                && matrix[4] != null && matrix[4].getType() == Material.LEATHER_BOOTS
                 && (matrix[6] == null || matrix[6].getType() == Material.AIR)
                 && (matrix[7] == null || matrix[7].getType() == Material.AIR)
                 && (matrix[8] == null || matrix[8].getType() == Material.AIR))
             event.getInventory().setResult(new ItemStack(Material.CHAINMAIL_BOOTS));
 
-        if((matrix[3] != null && matrix[3].getType() == Material.LAVA_BUCKET
-                && matrix[4] != null && matrix[4].getType() == Material.LAVA_BUCKET
-                && matrix[5] != null && matrix[5].getType() == Material.LAVA_BUCKET
-                && matrix[6] != null && matrix[6].getType() == Material.LAVA_BUCKET
-                && matrix[8] != null && matrix[8].getType() == Material.LAVA_BUCKET
-                && (matrix[0] == null || matrix[0].getType() == Material.AIR)
-                && (matrix[1] == null || matrix[1].getType() == Material.AIR)
-                && (matrix[2] == null || matrix[2].getType() == Material.AIR)
-                && (matrix[7] == null || matrix[7].getType() == Material.AIR))
-                || (matrix[0] != null && matrix[0].getType() == Material.LAVA_BUCKET
+        if((matrix[0] != null && matrix[0].getType() == Material.LAVA_BUCKET
                 && matrix[1] != null && matrix[1].getType() == Material.LAVA_BUCKET
                 && matrix[2] != null && matrix[2].getType() == Material.LAVA_BUCKET
                 && matrix[3] != null && matrix[3].getType() == Material.LAVA_BUCKET
                 && matrix[5] != null && matrix[5].getType() == Material.LAVA_BUCKET)
-                && (matrix[4] == null || matrix[4].getType() == Material.AIR)
+                && matrix[4] != null && matrix[4].getType() == Material.LEATHER_HELMET
                 && (matrix[6] == null || matrix[6].getType() == Material.AIR)
                 && (matrix[7] == null || matrix[7].getType() == Material.AIR)
                 && (matrix[8] == null || matrix[8].getType() == Material.AIR))
@@ -65,7 +58,7 @@ public class ItemListener implements Listener {
         if(matrix[0] != null && matrix[0].getType() == Material.LAVA_BUCKET
                 && matrix[2] != null && matrix[2].getType() == Material.LAVA_BUCKET
                 && matrix[3] != null && matrix[3].getType() == Material.LAVA_BUCKET
-                && matrix[4] != null && matrix[4].getType() == Material.LAVA_BUCKET
+                && matrix[4] != null && matrix[4].getType() == Material.LEATHER_CHESTPLATE
                 && matrix[5] != null && matrix[5].getType() == Material.LAVA_BUCKET
                 && matrix[6] != null && matrix[6].getType() == Material.LAVA_BUCKET
                 && matrix[7] != null && matrix[7].getType() == Material.LAVA_BUCKET
@@ -80,9 +73,175 @@ public class ItemListener implements Listener {
                 && matrix[5] != null && matrix[5].getType() == Material.LAVA_BUCKET
                 && matrix[6] != null && matrix[6].getType() == Material.LAVA_BUCKET
                 && matrix[8] != null && matrix[8].getType() == Material.LAVA_BUCKET
-                && (matrix[4] == null || matrix[4].getType() == Material.AIR)
+                && matrix[4] != null && matrix[4].getType() == Material.LEATHER_LEGGINGS
                 && (matrix[7] == null || matrix[7].getType() == Material.AIR))
             event.getInventory().setResult(new ItemStack(Material.CHAINMAIL_LEGGINGS));
+        //endregion
+
+        //region iron_armor
+        if((matrix[3] != null && matrix[3].getType() == Material.IRON_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.IRON_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.IRON_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.IRON_BLOCK
+                && (matrix[0] == null || matrix[0].getType() == Material.AIR)
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR)
+                && (matrix[2] == null || matrix[2].getType() == Material.AIR)
+                && (matrix[4] != null && matrix[4].getType() == Material.CHAINMAIL_BOOTS)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR))
+                || (matrix[0] != null && matrix[0].getType() == Material.IRON_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.IRON_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.IRON_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.IRON_BLOCK)
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR)
+                && (matrix[4] != null && matrix[4].getType() == Material.CHAINMAIL_BOOTS)
+                && (matrix[6] == null || matrix[6].getType() == Material.AIR)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR)
+                && (matrix[8] == null || matrix[8].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.IRON_BOOTS));
+
+        if((matrix[0] != null && matrix[0].getType() == Material.IRON_BLOCK
+                && matrix[1] != null && matrix[1].getType() == Material.IRON_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.IRON_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.IRON_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.IRON_BLOCK)
+                && (matrix[4] != null && matrix[4].getType() == Material.CHAINMAIL_HELMET)
+                && (matrix[6] == null || matrix[6].getType() == Material.AIR)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR)
+                && (matrix[8] == null || matrix[8].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.IRON_HELMET));
+
+        if(matrix[0] != null && matrix[0].getType() == Material.IRON_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.IRON_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.IRON_BLOCK
+                && matrix[4] != null && matrix[4].getType() == Material.CHAINMAIL_CHESTPLATE
+                && matrix[5] != null && matrix[5].getType() == Material.IRON_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.IRON_BLOCK
+                && matrix[7] != null && matrix[7].getType() == Material.IRON_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.IRON_BLOCK
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.IRON_CHESTPLATE));
+
+        if(matrix[0] != null && matrix[0].getType() == Material.IRON_BLOCK
+                && matrix[1] != null && matrix[1].getType() == Material.IRON_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.IRON_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.IRON_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.IRON_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.IRON_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.IRON_BLOCK
+                && (matrix[4] != null && matrix[4].getType() == Material.CHAINMAIL_LEGGINGS)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.IRON_LEGGINGS));
+        //endregion
+
+        //region gold_armor
+        if((matrix[3] != null && matrix[3].getType() == Material.GOLD_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.GOLD_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.GOLD_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.GOLD_BLOCK
+                && (matrix[0] == null || matrix[0].getType() == Material.AIR)
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR)
+                && (matrix[2] == null || matrix[2].getType() == Material.AIR)
+                && (matrix[4] != null && matrix[4].getType() == Material.IRON_BOOTS)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR))
+                || (matrix[0] != null && matrix[0].getType() == Material.GOLD_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.GOLD_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.GOLD_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.GOLD_BLOCK)
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR)
+                && (matrix[4] != null && matrix[4].getType() == Material.IRON_BOOTS)
+                && (matrix[6] == null || matrix[6].getType() == Material.AIR)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR)
+                && (matrix[8] == null || matrix[8].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.GOLDEN_BOOTS));
+
+        if((matrix[0] != null && matrix[0].getType() == Material.GOLD_BLOCK
+                && matrix[1] != null && matrix[1].getType() == Material.GOLD_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.GOLD_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.GOLD_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.GOLD_BLOCK)
+                && (matrix[4] != null && matrix[4].getType() == Material.IRON_HELMET)
+                && (matrix[6] == null || matrix[6].getType() == Material.AIR)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR)
+                && (matrix[8] == null || matrix[8].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.GOLDEN_HELMET));
+
+        if(matrix[0] != null && matrix[0].getType() == Material.GOLD_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.GOLD_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.GOLD_BLOCK
+                && matrix[4] != null && matrix[4].getType() == Material.IRON_CHESTPLATE
+                && matrix[5] != null && matrix[5].getType() == Material.GOLD_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.GOLD_BLOCK
+                && matrix[7] != null && matrix[7].getType() == Material.GOLD_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.GOLD_BLOCK
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.GOLDEN_CHESTPLATE));
+
+        if(matrix[0] != null && matrix[0].getType() == Material.GOLD_BLOCK
+                && matrix[1] != null && matrix[1].getType() == Material.GOLD_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.GOLD_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.GOLD_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.GOLD_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.GOLD_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.GOLD_BLOCK
+                && (matrix[4] != null && matrix[4].getType() == Material.IRON_LEGGINGS)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.GOLDEN_LEGGINGS));
+        //endregion
+
+        //region diamond_armor
+        if((matrix[3] != null && matrix[3].getType() == Material.DIAMOND_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.DIAMOND_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.DIAMOND_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.DIAMOND_BLOCK
+                && (matrix[0] == null || matrix[0].getType() == Material.AIR)
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR)
+                && (matrix[2] == null || matrix[2].getType() == Material.AIR)
+                && (matrix[4] != null && matrix[4].getType() == Material.GOLDEN_BOOTS)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR))
+                || (matrix[0] != null && matrix[0].getType() == Material.DIAMOND_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.DIAMOND_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.DIAMOND_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.DIAMOND_BLOCK)
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR)
+                && (matrix[4] != null && matrix[4].getType() == Material.GOLDEN_BOOTS)
+                && (matrix[6] == null || matrix[6].getType() == Material.AIR)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR)
+                && (matrix[8] == null || matrix[8].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.DIAMOND_BOOTS));
+
+        if((matrix[0] != null && matrix[0].getType() == Material.DIAMOND_BLOCK
+                && matrix[1] != null && matrix[1].getType() == Material.DIAMOND_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.DIAMOND_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.DIAMOND_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.DIAMOND_BLOCK)
+                && (matrix[4] != null && matrix[4].getType() == Material.GOLDEN_HELMET)
+                && (matrix[6] == null || matrix[6].getType() == Material.AIR)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR)
+                && (matrix[8] == null || matrix[8].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.DIAMOND_HELMET));
+
+        if(matrix[0] != null && matrix[0].getType() == Material.DIAMOND_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.DIAMOND_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.DIAMOND_BLOCK
+                && matrix[4] != null && matrix[4].getType() == Material.GOLDEN_CHESTPLATE
+                && matrix[5] != null && matrix[5].getType() == Material.DIAMOND_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.DIAMOND_BLOCK
+                && matrix[7] != null && matrix[7].getType() == Material.DIAMOND_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.DIAMOND_BLOCK
+                && (matrix[1] == null || matrix[1].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.DIAMOND_CHESTPLATE));
+
+        if(matrix[0] != null && matrix[0].getType() == Material.DIAMOND_BLOCK
+                && matrix[1] != null && matrix[1].getType() == Material.DIAMOND_BLOCK
+                && matrix[2] != null && matrix[2].getType() == Material.DIAMOND_BLOCK
+                && matrix[3] != null && matrix[3].getType() == Material.DIAMOND_BLOCK
+                && matrix[5] != null && matrix[5].getType() == Material.DIAMOND_BLOCK
+                && matrix[6] != null && matrix[6].getType() == Material.DIAMOND_BLOCK
+                && matrix[8] != null && matrix[8].getType() == Material.DIAMOND_BLOCK
+                && (matrix[4] != null && matrix[4].getType() == Material.GOLDEN_LEGGINGS)
+                && (matrix[7] == null || matrix[7].getType() == Material.AIR))
+            event.getInventory().setResult(new ItemStack(Material.DIAMOND_LEGGINGS));
+        //endregion
     }
 
     @EventHandler
