58d1906ddec4d82df8d008192584742c842a07c6
gabriel charry <gabriel.charry@gmail.com>
2024-12-28 16:54:46 +0100

    supprime le stuff des coffres du nether

--- src/main/java/fr/miuby/survi/listener/WorldListener.java
+++ src/main/java/fr/miuby/survi/listener/WorldListener.java
@@ -3,10 +3,16 @@ package fr.miuby.survi.listener;
 import fr.miuby.survi.player.AlphaPlayer;
 import fr.miuby.survi.world.EWorld;
 import fr.miuby.survi.world.Monde;
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.PortalCreateEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class WorldListener implements Listener {
     @EventHandler
@@ -23,5 +29,27 @@ public class WorldListener implements Listener {
     @EventHandler
     public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
         AlphaPlayer.get(event.getPlayer().getUniqueId()).switchWorld();
+    }
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent event) {
+        if (event.getWorld() != Monde.get(EWorld.NETHER).getWorld())
+            return;
+
+        for (BlockState blockState : event.getChunk().getTileEntities()) {
+            if (blockState instanceof Chest chest) {
+                Inventory inventory = chest.getInventory();
+
+                for (ItemStack stack : inventory.getContents()) {
+                    if (stack != null
+                            && (stack.getType() == Material.DIAMOND_BOOTS
+                            || stack.getType() == Material.DIAMOND_CHESTPLATE
+                            || stack.getType() == Material.DIAMOND_HELMET
+                            || stack.getType() == Material.DIAMOND_LEGGINGS)) {
+                        inventory.remove(stack);
+                    }
+                }
+            }
+        }
     }
 }
