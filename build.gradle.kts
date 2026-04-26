2cb950aa7e31c837c41b3fcc50b3ea1e31ecfdf3
charry-gabriel <gabriel.charry@gmail.com>
2026-04-26 16:09:45 +0200

    Add test to Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.1.2.build.5-alpha")
+    testImplementation("io.papermc.paper:paper-api:26.1.2.build.5-alpha")
 
     compileOnly("org.projectlombok:lombok:1.18.38")
     annotationProcessor("org.projectlombok:lombok:1.18.38")
@@ -31,6 +32,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.yaml:snakeyaml:2.3")
 }
 
 tasks {
