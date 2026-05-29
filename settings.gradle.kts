4aa8c2cb9cd8680c3eb6eea77b5cc10321ce0b3d
charry-gabriel <gabriel.charry@gmail.com>
2026-05-29 18:38:18 +0200

    Added MiubyLib local build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "Survi"
+
+val miubyLibDir = file("../MiubyLib")
+if (miubyLibDir.exists()) {
+    includeBuild("../MiubyLib") {
+        dependencySubstitution {
+            substitute(module("com.github.charry-gabriel:MiubyLib")).using(project(":"))
+        }
+    }
+}
